5f2597487498881aed1309e5fb98f1dd231810f8
Danmark_Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2025-01-09 16:30:55 +0900

    Detail : 수정 / 삭제 링크 수정
    수정 삭제 링크가 이상해서 badRequest뜨는 것 수정

--- src/main/java/com/example/_2025_bucket/controller/LikeController.java
+++ src/main/java/com/example/_2025_bucket/controller/LikeController.java
@@ -43,7 +43,7 @@ public class LikeController {
                 this.likeService.remove(likeDto.toEntity());
             }
         }catch (Exception e){
-            
+
         }
         return "redirect:/list/detail/" + id;
     }
